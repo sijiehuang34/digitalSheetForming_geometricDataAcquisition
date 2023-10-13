05ea2a78844dfe9f0b025b99acfc441252e39c90
sijiehuang34 <145233557+sijiehuang34@users.noreply.github.com>
2023-10-13 04:36:27 -0400

    Version 4
    Contains the part drawing the dots

--- cdFunc.m
+++ cdFunc.m
@@ -19,14 +19,20 @@ r_max_metric = 0.9; %cm
 Rmin = round((r_min_metric/imgLength)*height);
 Rmax = round((r_max_metric/imgLength)*height);
 
+metal_tn_metric = 0.25; %cm
+metal_tn = round((metal_tn_metric/imgLength)*height); %pixels
+
 %% Loop for circle identification
 for i = 1:nFrames
+
+    % Circle Identification
     imshow(cannyMat{i})
     [centers, radii, metric] = imfindcircles(cannyMat{i}, [Rmin Rmax]);
     centerArray = [];
     radiiArray = [];
     middleCircles = [];
     middleRadii = [];
+    dotArray = zeros(3,2);
     for j = 1:length(centers)
         if (centers(j,1)<width/3) || (centers(j,1)>(width*(2/3)))
             centerArray = [centerArray; centers(j,:)];
@@ -46,6 +52,39 @@ for i = 1:nFrames
         end
     end
     viscircles(centerArray, radiiArray,'EdgeColor','b');
+
+    % Dot location
+    for k = 1:3
+        % Left dot
+        if (centerArray(k,1) < width/3)
+            dot_x = centerArray(k,1); 
+            dot_y = centerArray(k,2) - radiiArray(k,1) - (metal_tn/2); 
+            dotArray(k,1) = dot_x;
+            dotArray(k,2) = dot_y;
+            dot_color = 'r';  
+            hold on;  
+            plot(dot_x, dot_y, 'o', 'MarkerSize', 5, 'MarkerFaceColor', dot_color, 'MarkerEdgeColor', dot_color);
+        % Right dot
+        elseif (centerArray(k,1) > width*(2/3))
+            dot_x = centerArray(k,1); 
+            dot_y = centerArray(k,2) - radiiArray(k,1) - (metal_tn/2);  
+            dotArray(k,1) = dot_x;
+            dotArray(k,2) = dot_y;
+            dot_color = 'r'; 
+            hold on;  
+            plot(dot_x, dot_y, 'o', 'MarkerSize', 5, 'MarkerFaceColor', dot_color, 'MarkerEdgeColor', dot_color);
+        % Middle dot
+        else
+            dot_x = centerArray(k,1);  
+            dot_y = centerArray(k,2) + radiiArray(k,1) + (metal_tn/1.5);  
+            dotArray(k,1) = dot_x;
+            dotArray(k,2) = dot_y;
+            dot_color = 'r';  
+            hold on;  
+            plot(dot_x, dot_y, 'o', 'MarkerSize', 5, 'MarkerFaceColor', dot_color, 'MarkerEdgeColor', dot_color);
+        end
+    
+    end
     
     % Save the processed image with a file name and store it in the matrix
     fileName = sprintf('%s/cdImage%04d.png', cd_Directory, j-1);
